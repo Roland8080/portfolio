d663d88b9d2753a17bb235966883e944cf54c054
Rolly Rollz <rollyrollz@ANGELIKA-MACBOOK-PRO.local>
2024-11-27 04:13:42 -0800

    first commit

---
+++ class.html/example.js
@@ -0,0 +1,65 @@
+//arithmetic operators
+let sum = 65 % 7;
+console.log(sum)
+
+let p = 6;
+++p
+
+console.log(p)
+
+//p = p + 6;
+
+p += 6;
+
+console.log(p)
+
+p -= 8;
+console.log(p)
+
+--p;
+console.log(p)
+
+p *= 16;
+console.log(p)
+
+//increment and decrement operators 
+let d = 10;
+
+console.log(++d)
+console.log(d)
+
+let s = 13;
+
+console.log(s++)
+console.log(s)
+
+//comparisim operators
+
+let x = 5;
+let y ='10';
+console.log(x == y);
+
+// logical operators AND OR NOT
+
+let v = 5;
+let f = 12;
+console.log(v > 4 && f >10);
+
+let yes = true;
+let no = false;
+console.log(!yes)
+console.log(!no)
+
+//  J.S string operators
+
+let o ="Hello ";
+let t ="world";
+
+console.log(o + t);
+
+ //strings and templates literals
+
+ const id = 'DOKU!';
+ const wel = `welcome ${id}`;
+
+ console.log(wel)
